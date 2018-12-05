1cde7b8d41192bdf07d150f10232675fffa5c5c4
khenryjoe <joseph.ketterer@gmail.com>
2018-12-05 14:45:22 +0000

    css grid image showcase for index page

---
+++ diabetes.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+function init() {
+    document.getElementById('health').onsubmit = submitForm;
+
+}
+
+function submitForm(){
+document.getElementByName(''); //returns a node list
+}
