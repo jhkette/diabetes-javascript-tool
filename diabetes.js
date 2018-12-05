1756a794e572e4bb9b855936ac332a93634aecaf
khenryjoe <joseph.ketterer@gmail.com>
2018-12-05 18:03:56 +0000

    add javascript foreach loop to loop through all values in form

--- diabetes.js
+++ diabetes.js
@@ -5,11 +5,54 @@ function init() {
 
 }
 
-function submitForm(e){
- var age = document.getElementsByName('age'); //returns a node list
-if(age[1].checked==true){
-console.log(age[1].value);
-console.log('hello');
-}
-e.preventDefault();
+// function submitForm(e){
+//  var age = document.getElementsByName('age'); //returns a node list
+//  for (i= 0; i < age.length; i++){
+// if(age[i].checked==true){
+// console.log(age[i].value);
+// console.log('hello');
+// }
+// }
+// e.preventDefault();
+// }
+
+function submitForm(e) {
+    var values = [];
+    var age = document.getElementsByName('age'); //returns a node list
+    age.forEach(function(age) {
+        if (age.checked == true) {
+            console.log(age.value);
+            values.push(age.value);
+            console.log(values);
+
+        }
+    });
+    var diet = document.getElementsByName('diet'); //returns a node list
+    diet.forEach(function(diet) {
+        if (diet.checked == true) {
+            console.log(diet.value);
+             values.push(diet.value);
+                console.log(values);
+        }
+    });
+
+    var family = document.getElementsByName('family'); //returns a node list
+    family.forEach(function(element) {
+        if (element.checked == true) {
+
+            values.push(element.value);
+
+                console.log(values);
+        }
+    });
+
+    var bmi = document.getElementsByName('bmi'); //returns a node list
+    bmi.forEach(function(element) {
+        if (element.checked == true) {
+            values.push(element.value);
+            console.log('hello')
+        }
+    });
+console.log(values);
+    e.preventDefault();
 }
