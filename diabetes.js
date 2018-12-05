1e862bfa3f5e657b90a5cafa0971ddac82618be1
khenryjoe <joseph.ketterer@gmail.com>
2018-12-05 16:05:29 +0000

    add some images/ start js

--- diabetes.js
+++ diabetes.js
@@ -1,10 +1,18 @@
 window.onload = init;
 
 function init() {
-    document.getElementById('health').onsubmit = submitForm;
+    document.getElementById('submit').onsubmit = submitForm();
 
 }
 
 function submitForm(){
-document.getElementByName(''); //returns a node list
+// var age = document.getElementsByName('age'); //returns a node list
+// for (i=0; i < age.length; i++) {
+//             if (age[i] == age[i].checked) {
+//             console.log(age[i].input.value);
+//         }
+//     }
+
+console.log('hello');
+return false;
 }
