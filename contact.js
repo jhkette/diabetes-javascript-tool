b8a098b4dd54cf26937dfd611d0303f4e72390f2
khenryjoe <joseph.ketterer@gmail.com>
2018-12-12 10:11:37 +0000

    used 'this' to simplify clear error function

--- contact.js
+++ contact.js
@@ -9,7 +9,7 @@ turn validate or report errors. */
 
 /* you are also going to need to change the background color of the first name
  part of the conctact form  */
-/* load the tooltip function here */
+
 
 function start() {
     firstNameHint();
@@ -21,9 +21,24 @@ function start() {
 
 function loadEventListeners() {
 
-    document.getElementById('userInfo').onsubmit = processForm;
-    document.getElementById('email').onfocus = clearError;
+     var email = document.getElementById('email');
+     // anonymous function to call clear error on focus with the id of email as an argument
+     email.onfocus = function() {
+         email = this.id;
+         clearError(email);
+     }
+
+     var telephone = document.getElementById('telephone');
+
+     telephone.onfocus = function() {
+         telephone = this.id;
+         clearError(telephone);
+     }
+
+    document.getElementById('telephone').onblur = validateTelephone;
     document.getElementById('email').onblur = validateEmail;
+
+    document.getElementById('userInfo').onsubmit = processForm;
 
 }
 
@@ -57,14 +72,9 @@ function processForm() {
 }
 
 
-function clearError() {
+function clearError(id) {
 
-    //clears element if it was an error Only on focus
-    document.getElementById('first-nameError').innerHTML = "&nbsp;";
-    document.getElementById('second-nameerror').innerHTML = "&nbsp;";
-    document.getElementById('health-authorityerror').innerHTML = "&nbsp;";
-    document.getElementById('emailError').innerHTML = "&nbsp;";
-    // document.getElementById(this.id + 'Error').innerHTML = "&nbsp;";
+    document.getElementById(id + 'Error').innerHTML = "&nbsp;";
 }
 
 
@@ -91,17 +101,19 @@ function validateFirstName() {
 
 
 function validateSecondName() {
+
     var defaultText = "Enter your name.";
     var valid = true;
     var secondName = document.getElementById('second-name').value;
-    /* second name contains only letters or a hyphen, more thah 2 letters, case insensitive */
-    var re = new RegExp(/^[a-z-]{2,}$/i);
+    console.log(secondName);
+    /* first name contain only letters and is at least two charecters long, case insensitive  */
+    var re = new RegExp(/^[a-z]{2,}$/i);
     if (re.test(secondName)) {
+        console.log('REG EX WORKED')
         return valid;
     } else {
-        document.getElementById('second-nameerror').innerHTML = 'error in the email field';
-        document.getElementById('second-name').style.border = "thin solid #0000FF";
-        secondNameHint();
+        document.getElementById('second-nameError').innerHTML = 'error in the name field';
+        secondNameHint()
         return valid = false;
     }
 }
@@ -109,10 +121,7 @@ function validateSecondName() {
 function validateEmail() {
     var valid = true;
     var email = document.getElementById('email').value;
-    /*does not include @
-    does include @
-    next charexter does not include a dot
-    does include a dot */
+
     /* https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149   */
     var re = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
     if (re.test(email)) {
@@ -124,18 +133,40 @@ function validateEmail() {
     }
 }
 
-function validateHealthAuthority() {
-    var defaultText = "Health number";
+function validateTelephone() {
     var valid = true;
-    var healthAuthority = document.getElementById('health').value;
-    var re = new RegExp(/^zha\d{6}$/i);
-    if (re.test(healthAuthority)) {
+    var telephone = document.getElementById('telephone').value;
+
+    /* https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149   */
+    var re = new RegExp(/^\d{11}$/i);
+    if (re.test(telephone)) {
         return valid;
     } else {
-        document.getElementById('health-authorityerror').innerHTML = 'error in the health field';
+        document.getElementById('telephoneError').innerHTML = 'error in the name field';
+
         return valid = false;
     }
 }
+
+
+function validateHealthAuthority() {
+
+
+        var defaultText = "Enter health number.";
+        var valid = true;
+        var health = document.getElementById('health').value;
+        console.log(health);
+        /* first name contain only letters and is at least two charecters long, case insensitive  */
+        var re = new RegExp(/^zha\d{^}$/i);
+        if (re.test(health)) {
+            console.log('REG EX WORKED')
+            return valid;
+        } else {
+            document.getElementById('healthError').innerHTML = 'error in the name field';
+            healthHint()
+            return valid = false;
+        }
+    }
 
 function firstNameHint() {
     var defaultText = "Enter your name.";
@@ -151,8 +182,12 @@ function firstNameHint() {
             this.value = "";
             this.style.color = "#000";
             this.style.fontStyle = "normal";
-            clearError();
+
+
+
         }
+        textElem = this.id;
+        clearError(textElem);
 
     }
 
@@ -174,32 +209,37 @@ function secondNameHint() {
     txtElem.style.color = "#A8A8A8";
     txtElem.style.fontStyle = "italic";
 
-
     txtElem.onfocus = function() {
-        // the value being operated on
+        //the value being operated on
         if (this.value === defaultText) {
+
             this.value = "";
             this.style.color = "#000";
             this.style.fontStyle = "normal";
+
+
+
         }
-        clearError();
+        textElem = this.id;
+        clearError(textElem);
     }
+
     txtElem.onblur = function() {
         if (this.value === "") {
             this.value = defaultText;
             this.style.color = "#A8A8A8";
             this.style.fontStyle = "italic";
         }
-        // user moving away from form field will trigger form validation
         validateSecondName();
     }
 }
 
 
+
 function healthHint() {
     var defaultText = "Health number.";
     var txtElem = document.getElementById("health");
-    console.log(txtElem);
+    console.log(txtElem + 'HEALTH');
     txtElem.value = defaultText;
     txtElem.style.color = "#A8A8A8";
     txtElem.style.fontStyle = "italic";
@@ -209,12 +249,15 @@ function healthHint() {
 
         // the value being operated on
         if (this.value === defaultText) {
-            console.log('HELLLO')
+            console.log('HELLLO health')
             this.value = "";
             this.style.color = "#000";
             this.style.fontStyle = "normal";
+
         }
-        clearError();
+        textElem = this.id;
+        clearError(textElem);
+
     }
 
     txtElem.onblur = function() {
