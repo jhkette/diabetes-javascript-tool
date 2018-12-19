258ddad1f85b25044b891965faf691249a8b3b07
khenryjoe <joseph.ketterer@gmail.com>
2018-12-19 20:20:58 +0000

    added detailed notes to contact.js

--- contact.js
+++ contact.js
@@ -16,16 +16,17 @@ function start() {
     switchToolTip();
 }
 
-/*This function loads all the event listeners need on the contact form  */
+/*This function loads all the event listeners needed on the contact form  */
 function loadEventListeners() {
-    // anonymous function to call clear error on focus with the id of email as an argument
     var email = document.getElementById('email');
+    // anonymous function to call clear error on focus with the id of email as an argument
     email.onfocus = function() {
         email = this.id;
         clearError(email);
     };
-    // anonymous function to call clear error on focus with the id of telephone as an argument
+
     var telephone = document.getElementById('telephone');
+    // anonymous function to call clear error on focus with the id of telephone as an argument
     telephone.onfocus = function() {
         telephone = this.id;
         clearError(telephone);
@@ -38,25 +39,27 @@ function loadEventListeners() {
     document.getElementById('userInfo').onsubmit = processForm;
 
 }
-
+/* This function processes form. It's called on form submission */
 function processForm() {
-
+    /* assign validation functions to variable */
     var firstName = validateFirstName();
     var lastName = validateSecondName();
     var email = validateEmail();
     var health = validateHealthAuthority();
-
+    /* if statement to check validation functions return true (ie fields are valid) */
     if ((firstName == true) && (lastName == true) && (email == true) && (health == true)) {
+        /* call modal if all true */
         toggleModal();
-        return false;
+        return false; // stop form submitting
     } else {
+        /* add error message below form */
         document.getElementById('submitError').innerHTML = 'There are errors in the form';
-        return false;
+        return false; // stop form submitting
     }
 }
 
-/* Clear error function which takes the parameter of 'id' of the form field to add an error messsage beside the
-form field */
+/* Clear error function which takes the parameter of the 'id' of the form field to add an error messsage beside the
+form field. This works as the id of the error fields is the same as the form field but with an added 'Error' at the end */
 function clearError(id) {
     document.getElementById(id + 'Error').innerHTML = "&nbsp;";
     document.getElementById('submitError').innerHTML = "&nbsp;";
@@ -69,13 +72,12 @@ function removeNameFocus() {
     firstNameField.classList.remove('focus');
 }
 
-/* This add a red background to the form if there is an error. It gets called in the validation functions .  */
+/* This add a red background to the form if there is an error in the form. It gets called in the validation functions .  */
 function addRedError(field) {
     field.classList.add('backgroundred');
 }
 
-
-/*This removes the function if the form is correct. It is called in the validation functions    */
+/*This removes the red background if the form field is correct. It is called in the validation functions    */
 function removeRedError(field) {
     field.classList.remove('backgroundred');
 }
@@ -88,33 +90,36 @@ function validateFirstName() {
     var defaultText = "Enter your name.";
     var valid = true;
     var firstNameField = document.getElementById('first-name');
-    var firstName = document.getElementById('first-name').value;
 
     /* first name contain only letters and is at least two charecters long, case insensitive  */
     var re = new RegExp(/^[a-z]{2,}$/i);
-    if (re.test(firstName)) {
+    if (re.test(firstNameField.value)) { // test value against regular expression
+        /* Remove initial focus on first name */
         removeNameFocus();
+        /* Remove error background if it exists (maybe add if statement??) */
         removeRedError(firstNameField);
-        return valid;
+        return valid; // return valid
     } else {
         document.getElementById('first-nameError').innerHTML = 'error in the name field';
+         /* Remove initial focus on first name */
         removeNameFocus();
+        /* Add red error background */
         addRedError(firstNameField);
-        valid = false;
+        valid = false; // change valid to false
         return valid;
     }
 }
-
+/* Function to validate last name. The following validation functions follow the same structure
+as the first. They obviously need to be seperate functions, as we are validating different values
+against differnt regular expressions. */
 function validateSecondName() {
-
     var defaultText = "Enter your name.";
     var valid = true;
     var secondNameField = document.getElementById('second-name');
-    var secondName = document.getElementById('second-name').value;
 
     /* first name contain only letters or '-'. It is at least two charecters long, case insensitive  */
     var re = new RegExp(/^[a-z-]{2,}$/i);
-    if (re.test(secondName)) {
+    if (re.test(secondNameField.value)) {
         removeRedError(secondNameField);
         return valid;
     } else {
@@ -125,32 +130,34 @@ function validateSecondName() {
     }
 }
 
+/* Validate email function */
 function validateEmail() {
     var valid = true;
     var emailField = document.getElementById('email');
-    var email = document.getElementById('email').value;
 
-    /* https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149   */
-    var re = new RegExp(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
-    if (re.test(email)) {
-        removeRedError(emailField);
+    /* regular expression: one or more letters or numbers or '_.-', followed by an @ sign. Then the email provider, which is letters,
+    numbers, or selected punctuation. Then a dot. Then a domain name which is letters, may contain a dot. Between 2 and 6 chrecters long.
+    Then end of string.
+    Inspiration from https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149 */
+    var re = new RegExp(/^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/);
+    if (re.test(emailField.value)) {
+        removeRedError(emailField); // remove red error
         return valid;
     } else {
-        addRedError(emailField);
-        document.getElementById('emailError').innerHTML = 'error in the name field';
+        addRedError(emailField); // add red error
+        document.getElementById('emailError').innerHTML = 'error in the name field'; // add error message
         valid = false;
         return valid;
     }
 }
 
+/* function to validation telephone */
 function validateTelephone() {
     var valid = true;
     var telephoneField = document.getElementById('telephone');
-    var telephone = document.getElementById('telephone').value;
-
-    /* https://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149   */
+    /* regular expression to match 11 digits no other chrecters */
     var re = new RegExp(/^\d{11}$/i);
-    if (re.test(telephone)) {
+    if (re.test(telephoneField.value)) {
         removeRedError(telephoneField);
         return valid;
     } else {
@@ -161,17 +168,15 @@ function validateTelephone() {
     }
 }
 
-
+/* Function to validate health authority field */
 function validateHealthAuthority() {
 
     var defaultText = "Enter health number.";
     var valid = true;
     var healthField = document.getElementById('health');
-    var health = document.getElementById('health').value;
-
-    /* first name contain only letters and is at least two charecters long, case insensitive  */
-    var re = new RegExp(/^zha\d{6}$/i);
-    if (re.test(health)) {
+    /* regular expression starts with zha and is then followed by 6 digits. case insensitive  */
+    var re = new RegExp(/^(zha)(\d{6})$/i);
+    if (re.test(healthField.value)) {
         removeRedError(healthField);
         return valid;
     } else {
@@ -181,46 +186,49 @@ function validateHealthAuthority() {
         return valid;
     }
 }
-
+/* Function to add a hint to the first name field */
 function firstNameHint() {
-    var defaultText = "Enter your name.";
-    var txtElem = document.getElementById("first-name");
-    txtElem.value = defaultText;
-    txtElem.style.color = "#A8A8A8";
+    var defaultText = "Enter your first name."; // defualt text to be entered
+    var txtElem = document.getElementById("first-name"); //asign field to variable
+    txtElem.value = defaultText; // add default text and styling
+    txtElem.style.color = "#aba9a9";
     txtElem.style.fontStyle = "italic";
 
     txtElem.onfocus = function() {
-        //the value being operated on
+        /* If value equals default text onfocus  - replace with empty string. (ie remove default text) to
+        allow user to type into field */
         if (this.value === defaultText) {
-
             this.value = "";
             this.style.color = "#000";
             this.style.fontStyle = "normal";
         }
 
-        textElemId = this.id;
-        clearError(textElemId);
+        textElemId = this.id; // assign id of value being operated on to variable
+        clearError(textElemId); /*as the user is 'focused' on the form
+        we need to call clear error function with id of value as an argument. This removes prior
+        error message */
     };
     txtElem.onblur = function() {
-        if (this.value === "") {
+        if (this.value === "") { // on blur we readd default text if the value is empty
             this.value = defaultText;
-            this.style.color = "#A8A8A8";
+            this.style.color = "#aba9a9";
             this.style.fontStyle = "italic";
         }
+        /* We also need to call the validate function. The user has engaged then
+        moved away from the form, so we need to check if the input was valid   */
         validateFirstName();
     };
 }
 
-
+/* Function to add a last name hint, this follows the same structure as the first name */
 function secondNameHint() {
-    var defaultText = "Enter your name.";
+    var defaultText = "Enter your last name.";
     var txtElem = document.getElementById("second-name");
     txtElem.value = defaultText;
-    txtElem.style.color = "#A8A8A8";
+    txtElem.style.color = "#aba9a9";
     txtElem.style.fontStyle = "italic";
 
     txtElem.onfocus = function() {
-        //the value being operated on
         if (this.value === defaultText) {
 
             this.value = "";
@@ -234,7 +242,7 @@ function secondNameHint() {
     txtElem.onblur = function() {
         if (this.value === "") {
             this.value = defaultText;
-            this.style.color = "#A8A8A8";
+            this.style.color = "#aba9a9";
 
             this.style.fontStyle = "italic";
         }
@@ -243,17 +251,16 @@ function secondNameHint() {
 }
 
 
-
+/* Function to add a health hint */
 function healthHint() {
-    var defaultText = "Health number.";
+    var defaultText = "Enter Health Authority Number.";
     var txtElem = document.getElementById("health");
     txtElem.value = defaultText;
-    txtElem.style.color = "#A8A8A8";
+    txtElem.style.color = "#aba9a9";
     txtElem.style.fontStyle = "italic";
     txtElem.style.fontFamily = "calibri";
     txtElem.onfocus = function() {
 
-        // the value being operated on
         if (this.value === defaultText) {
             this.value = "";
             this.style.color = "#000";
@@ -262,48 +269,49 @@ function healthHint() {
         }
         textElemId = this.id;
         clearError(textElemId);
-
     };
 
     txtElem.onblur = function() {
         if (this.value === "") {
             this.value = defaultText;
-            this.style.color = "#A8A8A8";
+            this.style.color = "#aba9a9";
             this.style.fontStyle = "italic";
         }
-        // user moving away from form field will trigger form validation
-
         validateHealthAuthority();
     };
 }
 
 
-/*Function to add tooltip. I'm changing the opacity on mouseout/mouseover.  */
+/* Function to add tooltip. I'm changing the opacity on mouseout/mouseover. */
 function switchToolTip() {
-    document.getElementById('qmark').onmouseover = function() {
+    document.getElementById('qmark').onmouseover = function() { // when user howver over question mark...
         var toolTip = document.getElementById('ttip');
-        toolTip.style.opacity = 1;
+        toolTip.style.opacity = 1; // change the opacity of the tooltip to 1
     };
-    document.getElementById('qmark').onmouseout = function() {
+    document.getElementById('qmark').onmouseout = function() { // when the user moves away
         var toolTip = document.getElementById('ttip');
-        toolTip.style.opacity = 0;
+        toolTip.style.opacity = 0; // change the tooltip opacity to 0
     };
 }
-
+/* Function to add a modal popup. It is called if the form is completed correctly
+some inspiration taken from here https://sabe.io/tutorials/how-to-create-modal-popup-box */
 function toggleModal() {
-    var modal = document.querySelector(".modal");
-    modal.classList.add("show-modal");
-    var closeButton = document.querySelector(".close-button");
+    var modal = document.querySelector(".modal"); // assign modal class div to vairable
+    modal.classList.add("show-modal"); // add class (this changes opacity in css)
+    var closeButton = document.querySelector(".close-button"); // assign close button to variable
 
-    closeButton.addEventListener("click", removeModal);
-    window.addEventListener("click", windowOnClick);
+    closeButton.addEventListener("click", removeModal); // on click call removeModal
+    window.addEventListener("click", windowClick); // on click call windowOnClick
 
-    function windowOnClick(event) {
+    function windowClick(event) {
+        /* if the target of the event equals modal, remove modal. The target property gets the element on which the
+        event originally occurred, as opposed to the currentTarget property, which always refers to the element whose
+        event listener triggered the event.  */
         if (event.target === modal) {
             removeModal();
         }
     }
-
+    /* remove show modal class - this is called on the close button and a click outside window  */
     function removeModal(e) {
         var modal = document.querySelector(".modal");
         modal.classList.remove("show-modal");
