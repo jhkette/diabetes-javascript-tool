c1c5062f3826e9f9fcdd929d55d6aae413a05241
khenryjoe <joseph.ketterer@gmail.com>
2019-01-01 13:46:10 +0000

    shorten diabetes.js code by getting elements by classnae

--- diabetes.js
+++ diabetes.js
@@ -8,21 +8,26 @@ Clear form is called first in case there is still an input in the 'results' div.
 function init() {
     document.getElementById('Submit').addEventListener('click', clearForm);
     document.getElementById('Submit').addEventListener('click', calculateForm);
-    openListItems();
+    // openListItems();
 }
 
 /* clears the results form. It's called on every click of the 'calculate button' */
 function clearForm(e) {
     var divElement = document.querySelector('.results');
     divElement.innerHTML = '';
+    e.preventDefault();
 }
 
 /*  the submitForm function takes the values from the radio button form add adds them to two arrays - 'values' and 'warnings' */
 function calculateForm(e) {
     var values = []; // instantiate value array
     var warnings = []; // instantiate warning array
-    var age = document.getElementsByName('age'); //returns a node list of all items with name 'age'
-    age.forEach(function(element) { // loop through each element in the node list
+    var radio = document.querySelectorAll('.radio'); //returns a node list of all items with the class name 'radio'
+
+
+
+    radio.forEach(function(element) { // loop through each element in the node list
+
         if (element.checked == true) { // if the element is checked
             values.push(parseInt(element.value)); // push the value assigned to the radio button
             if (element.value >= 10) { // if greater or equal to 10 push the element NAME into the warning array.
@@ -30,37 +35,7 @@ function calculateForm(e) {
             }
         }
     });
-    /* each section of the radio button form is dealt with in the same way as above. The selected value is added
-    to the array. If over 10 the name is added to the warning array */
-    var diet = document.getElementsByName('diet'); //returns a node list of all items with name 'diet'
-    diet.forEach(function(element) { // loop through each element in the node list
-        if (element.checked == true) {
-            values.push(parseInt(element.value));
-            if (element.value >= 10) {
-                warnings.push(element.name);
-            }
-        }
-    });
 
-    var family = document.getElementsByName('family');
-    family.forEach(function(element) {
-        if (element.checked == true) {
-            values.push(parseInt(element.value));
-            if (element.value >= 10) {
-                warnings.push(element.name);
-            }
-        }
-    });
-
-    var bmi = document.getElementsByName('BMI');
-    bmi.forEach(function(element) {
-        if (element.checked == true) {
-            values.push(parseInt(element.value));
-            if (element.value >= 10) {
-                warnings.push(element.name);
-            }
-        }
-    });
     /* This function is used by the javascript higher order reduce function to get the sum of the values in the array  */
     function getSum(total, num) {
         return total + num; // accumulator + value
@@ -69,7 +44,8 @@ function calculateForm(e) {
     var finalNumber = values.reduce(getSum);
     // call function to calculateResults with the finalNumber and warning array as arguments
     calculateResults(finalNumber, warnings);
-    // prevent defualt behaviour (ie don't submit the form)
+    
+    // // prevent defualt behaviour (ie don't submit the form)
     e.preventDefault();
 }
 
@@ -78,7 +54,7 @@ function calculateResults(number, warnings) {
     /* delcare warningText variable */
     var warningText = '';
     /* a switch case statement that assigns a string to warningText based on the length of the 'warning' array
-    which was passed as an argument. This is only performed if the value of the number is OVER 25 */
+     which was passed as an argument. This is only performed if the value of the number is OVER 25 */
     if (number > 25) {
         switch (true) {
             case (warnings.length == 1):
@@ -102,31 +78,31 @@ function calculateResults(number, warnings) {
 
     /* A switch case statement that adds html to the parapgraph based on the number value. I'm using innerHTML here as opposed to creating
     a series of textnodes. Because there are links in two of the reponses this would involve creating text nodes for the text, then the link,
-    then the subsequent text. Using innerHTML in this instance creates far shorter and more legible code. */
-    switch(true){
+     then the subsequent text. Using innerHTML in this instance creates far shorter and more legible code. */
+    switch (true) {
         case number <= 15:
-        results.innerHTML = 'Your results show that you currently have a low risk of developing diabetes. It is important that you maintain a healthy lifestyle in terms of diet and exercise.';
-        break;
+            results.innerHTML = 'Your results show that you currently have a low risk of developing diabetes. It is important that you maintain a healthy lifestyle in terms of diet and exercise.';
+            break;
         case (number >= 16 && number <= 25):
-        results.innerHTML = 'Your results show that you currently have a medium risk of developing diabetes. For more information on your risk factors, and what to do about them, please visit our diabetes advice website at <a href="https://www.diabetes.org.uk/">http://www.zha.org.zd</a>.';
-        break;
+            results.innerHTML = 'Your results show that you currently have a medium risk of developing diabetes. For more information on your risk factors, and what to do about them, please visit our diabetes advice website at <a href="https://www.diabetes.org.uk/">http://www.zha.org.zd</a>.';
+            break;
         case number > 25:
-        results.innerHTML = 'Your results show that you currently have a high risk of developing diabetes.' + warningText +
-            'We advise that you contact the Health Authority to discuss your risk factors as soon as you can. Please fill in our <a href="contact.html">contact</a> form and a member of the Health Authority Diabetes Team will be in contact with you.';
+            results.innerHTML = 'Your results show that you currently have a high risk of developing diabetes.' + warningText +
+             'We advise that you contact the Health Authority to discuss your risk factors as soon as you can. Please fill in our <a href="contact.html">contact</a> form and a member of the Health Authority Diabetes Team will be in contact with you.';
     }
     // append the paragraph to the div
     divElement.appendChild(results);
 }
 
-/* This is a function to add a simple effect to the home page */
-/* I introduce the form with a flexbox list of diabetes factors. A click on a list item enlarges
-it by toggling a class with 'flex grow'*/
+// /* This is a function to add a simple effect to the home page */
+// /* I introduce the form with a flexbox list of diabetes factors. A click on a list item enlarges
+// it by toggling a class with 'flex grow'*/
 function openListItems() {
     // select all contentitems.This is now a node list of list items
     var contentitems = document.querySelectorAll('.contentitems');
     var links = document.querySelectorAll('.summary');
 
-     // function that toggles classList 'open'. This changes flexgrow property in css
+    // function that toggles classList 'open'. This changes flexgrow property in css
     function toggleOpen(e) {
 
         this.classList.toggle('open');
